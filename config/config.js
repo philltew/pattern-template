3bedc335a62194518d1a631438e3fff7ef3751c3
Joost De Cock <joost@decock.org>
2018-09-06 10:06:04 +0200

    :sparkles: Updated config format

--- config/config.js
+++ config/config.js
@@ -1,16 +1,15 @@
+// See config syntax documentation at:
+// https://developer.freesewing.org/config/
 export default {
   name: "template",
-  measurements: [
-    "chestCircumference",
-    "wristCircumference"
-  ],
+  measurements: ["chestCircumference", "wristCircumference"],
   options: {
     // Constants
     foo: 4,
     bar: 8,
 
     // Percentages
-    chestEase:              { val:   8, min: -4, max:  20 },
-    cuffEase:               { val:  20, min:  0, max: 200 },
+    chestEase: { pct: 8, min: -4, max: 20 },
+    cuffEase: { pct: 20, min: 0, max: 200 }
   }
 };
